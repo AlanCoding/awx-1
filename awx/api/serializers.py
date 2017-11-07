ansible/awx

consolidate management of lists of prompts

awx/api/serializers.py
@@ -38,6 +38,7 @@ from polymorphic.models import PolymorphicModel
 from awx.main.constants import SCHEDULEABLE_PROVIDERS, ANSI_SGR_PATTERN
 from awx.main.models import * # noqa
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.prompts import ask_mapping
 from awx.main.access import get_user_capabilities
 from awx.main.fields import ImplicitRoleField
 from awx.main.utils import (
@@ -3289,10 +3290,8 @@ class JobLaunchSerializer(BaseSerializer):
         return False
 
     def get_defaults(self, obj):
-        ask_for_vars_dict = obj._ask_for_vars_dict()
-        ask_for_vars_dict['vault_credential'] = False
         defaults_dict = {}
-        for field in ask_for_vars_dict:
+        for field in ask_mapping.keys():
             if field in ('inventory', 'credential', 'vault_credential'):
                 defaults_dict[field] = dict(
                     name=getattrd(obj, '%s.name' % field, None),
@@ -3320,7 +3319,7 @@ class JobLaunchSerializer(BaseSerializer):
         data = self.context.get('data')
 
         for field in obj.resources_needed_to_start:
-            if not (attrs.get(field, False) and obj._ask_for_vars_dict().get(field, False)):
+            if not (attrs.get(field, False) and getattr(obj, ask_mapping[field], False)):
                 errors[field] = _("Job Template '%s' is missing or undefined.") % field
 
         if obj.inventory and obj.inventory.pending_deletion is True:

awx/api/views.py
@@ -80,6 +80,7 @@ from awx.api.serializers import * # noqa
 from awx.api.metadata import RoleMetadata, JobTypeMetadata
 from awx.main.consumers import emit_channel_notification
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.prompts import ask_mapping
 from awx.main.scheduler.tasks import run_job_complete
 
 logger = logging.getLogger('awx.api.views')
@@ -2708,12 +2709,12 @@ class JobTemplateLaunch(RetrieveAPIView):
                 extra_vars.setdefault(v, u'')
             if extra_vars:
                 data['extra_vars'] = extra_vars
-            ask_for_vars_dict = obj._ask_for_vars_dict()
-            ask_for_vars_dict.pop('extra_vars')
+            modified_ask_mapping = ask_mapping.copy()
+            modified_ask_mapping.pop('extra_vars')
             if get_request_version(self.request) == 1:  # TODO: remove in 3.3
-                ask_for_vars_dict.pop('extra_credentials')
-            for field in ask_for_vars_dict:
-                if not ask_for_vars_dict[field]:
+                modified_ask_mapping.pop('extra_credentials')
+            for field in modified_ask_mapping.keys():
+                if not getattr(obj, modified_ask_mapping[field]):
                     data.pop(field, None)
                 elif field == 'inventory' or field == 'credential':
                     data[field] = getattrd(obj, "%s.%s" % (field, 'id'), None)

awx/main/access.py
@@ -25,6 +25,7 @@ from awx.main.utils import (
 )
 from awx.main.models import * # noqa
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.prompts import ask_mapping
 from awx.main.models.mixins import ResourceMixin
 
 from awx.conf.license import LicenseForbids, feature_enabled
@@ -1416,7 +1417,7 @@ class JobAccess(BaseAccess):
             prompts_access = True
             job_fields = {}
             jt_extra_credentials = set(obj.job_template.extra_credentials.all())
-            for fd in obj.job_template._ask_for_vars_dict():
+            for fd in ask_mapping.keys():
                 if fd == 'extra_credentials':
                     job_fields[fd] = job_extra_credentials
                 job_fields[fd] = getattr(obj, fd)

AlanCoding/awx-1

awx/api/serializers.py
@@ -38,6 +38,7 @@ from polymorphic.models import PolymorphicModel
 from awx.main.constants import SCHEDULEABLE_PROVIDERS, ANSI_SGR_PATTERN
 from awx.main.models import * # noqa
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.jobs import ask_mapping
 from awx.main.access import get_user_capabilities
 from awx.main.fields import ImplicitRoleField
 from awx.main.utils import (
@@ -3289,10 +3290,8 @@ class JobLaunchSerializer(BaseSerializer):
         return False
 
     def get_defaults(self, obj):
-        ask_for_vars_dict = obj._ask_for_vars_dict()
-        ask_for_vars_dict['vault_credential'] = False
         defaults_dict = {}
-        for field in ask_for_vars_dict:
+        for field in ask_mapping.keys():
             if field in ('inventory', 'credential', 'vault_credential'):
                 defaults_dict[field] = dict(
                     name=getattrd(obj, '%s.name' % field, None),
@@ -3320,7 +3319,7 @@ class JobLaunchSerializer(BaseSerializer):
         data = self.context.get('data')
 
         for field in obj.resources_needed_to_start:
-            if not (attrs.get(field, False) and obj._ask_for_vars_dict().get(field, False)):
+            if not (attrs.get(field, False) and getattr(obj, ask_mapping[field], False)):
                 errors[field] = _("Job Template '%s' is missing or undefined.") % field
 
         if obj.inventory and obj.inventory.pending_deletion is True:

awx/api/views.py
@@ -80,6 +80,7 @@ from awx.api.serializers import * # noqa
 from awx.api.metadata import RoleMetadata, JobTypeMetadata
 from awx.main.consumers import emit_channel_notification
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.jobs import ask_mapping
 from awx.main.scheduler.tasks import run_job_complete
 
 logger = logging.getLogger('awx.api.views')
@@ -2708,12 +2709,12 @@ class JobTemplateLaunch(RetrieveAPIView):
                 extra_vars.setdefault(v, u'')
             if extra_vars:
                 data['extra_vars'] = extra_vars
-            ask_for_vars_dict = obj._ask_for_vars_dict()
-            ask_for_vars_dict.pop('extra_vars')
+            modified_ask_mapping = ask_mapping.copy()
+            modified_ask_mapping.pop('extra_vars')
             if get_request_version(self.request) == 1:  # TODO: remove in 3.3
-                ask_for_vars_dict.pop('extra_credentials')
-            for field in ask_for_vars_dict:
-                if not ask_for_vars_dict[field]:
+                modified_ask_mapping.pop('extra_credentials')
+            for field in modified_ask_mapping.keys():
+                if not getattr(obj, modified_ask_mapping[field]):
                     data.pop(field, None)
                 elif field == 'inventory' or field == 'credential':
                     data[field] = getattrd(obj, "%s.%s" % (field, 'id'), None)

awx/main/access.py
@@ -25,6 +25,7 @@ from awx.main.utils import (
 )
 from awx.main.models import * # noqa
 from awx.main.models.unified_jobs import ACTIVE_STATES
+from awx.main.models.jobs import ask_mapping
 from awx.main.models.mixins import ResourceMixin
 
 from awx.conf.license import LicenseForbids, feature_enabled
@@ -1410,7 +1411,7 @@ class JobAccess(BaseAccess):
             prompts_access = True
             job_fields = {}
             jt_extra_credentials = set(obj.job_template.extra_credentials.all())
-            for fd in obj.job_template._ask_for_vars_dict():
+            for fd in ask_mapping.keys():
                 if fd == 'extra_credentials':
                     job_fields[fd] = job_extra_credentials
                 job_fields[fd] = getattr(obj, fd)