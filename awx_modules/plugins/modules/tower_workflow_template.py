8353fa9a0b0c95b03ae83314a52588d8a25dcbdd
AlanCoding <arominge@redhat.com>
2019-08-30 17:11:01 -0400

    Apply changes from content_collector tool

--- awx_modules/plugins/modules/tower_workflow_template.py
+++ awx_modules/plugins/modules/tower_workflow_template.py
@@ -94,7 +94,7 @@ RETURN = ''' # '''
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ansible_tower import (
+from ..module_utils.ansible_tower import (
     TowerModule,
     tower_auth_config,
     tower_check_mode
